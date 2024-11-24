4ae240c670356acaa4450ecd8d2593a4576c321d
MRPSouza <contato@matheusrpsouza.com>
2024-11-24 17:57:03 -0300

    2024/11/24 17:56 0.1.2

--- private/source/pages/layouts/components/html.head&startBody/php/security_headers.php
+++ private/source/pages/layouts/components/html.head&startBody/php/security_headers.php
@@ -1,17 +1,17 @@
 <?php
+// Definir origens permitidas
+$allowed_domains = [
+    'cdn.jsdelivr.net',
+    'cdnjs.cloudflare.com',
+    '*.cloudflare.com'
+];
+
+// Construir CSP
+$csp_domains = implode(' https://', array_merge([''], $allowed_domains));
+
 $csp = "default-src 'self'; " .
-    "script-src 'self' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com " . 
-    "'nonce-" . $nonces['content_dynamic'] . "' " .
-    "'nonce-" . $nonces['resize_body'] . "' " .
-    "'nonce-" . $nonces['iframe_restrict'] . "' " .
-    "'nonce-" . $nonces['tooltip'] . "' " .
-    "'nonce-" . $nonces['bootstrap_js'] . "' " .
-    "'nonce-" . $nonces['popper_js'] . "' " .
-    "'nonce-" . $nonces['page_styles'] . "'; " .
-    "style-src 'self' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com " .
-    "'nonce-" . $nonces['bootstrap_css'] . "' " .
-    "'nonce-" . $nonces['fontawesome'] . "' " .
-    "'nonce-" . $nonces['page_styles'] . "'; " .
+    "script-src 'self'" . $csp_domains . "; " .
+    "style-src 'self'" . $csp_domains . "; " .
     "font-src 'self' https://cdnjs.cloudflare.com data:; " .
     "img-src 'self' data:;";
 
