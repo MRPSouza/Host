82811853e85894174afee26b990ee43b0e724ddc
MRPSouza <contato@matheusrpsouza.com>
2024-11-24 00:07:00 -0300

    2024/11/24 00:06 0.1.2

--- private/source/pages/layouts/components/html.scripts&endBody.php
+++ private/source/pages/layouts/components/html.scripts&endBody.php
@@ -2,8 +2,8 @@
 global $nonce;
 
 # Chamadas dos scripts externos com SRI
-require_once '../private/source/pages/layouts/components/html.scripts&endBody/php/external_links.php';
-require_once '../private/source/pages/layouts/components/html.scripts&endBody/php/local_scripts.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../private/source/pages/layouts/components/html.scripts&endBody/php/external_links.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../private/source/pages/layouts/components/html.scripts&endBody/php/local_scripts.php';
 
 # Renderizar scripts externos
 foreach ($external_scripts as $script) {
@@ -12,12 +12,14 @@ foreach ($external_scripts as $script) {
         crossorigin="anonymous"></script>';
 }
 
-# Renderizar scripts locais - Agora lendo o conteúdo dos arquivos
+# Renderizar scripts locais - Usando caminho absoluto
 foreach ($local_scripts as $script) {
     $fullPath = $_SERVER['DOCUMENT_ROOT'] . '/../private/source/pages/js/' . basename($script['path']);
     if (file_exists($fullPath)) {
         $content = file_get_contents($fullPath);
         echo '<script nonce="' . $nonce . '">' . $content . '</script>';
+    } else {
+        error_log("Arquivo não encontrado: " . $fullPath);  // Log para debug
     }
 }
 
--- private/source/pages/layouts/components/html.scripts&endBody/php/local_scripts.php
+++ private/source/pages/layouts/components/html.scripts&endBody/php/local_scripts.php
@@ -1,23 +1,23 @@
 <?php
 $local_scripts = [
     'content_dynamic' => [
-        'path' => '/../private/source/pages/js/content_dynamic.js',
+        'path' => 'content_dynamic.js',
         'version' => '1.0'
     ],
     'resize_body_bootstrap' => [
-        'path' => '/../private/source/pages/js/resize_body_bootstrap.js',
+        'path' => 'esize_body_bootstrap.js',
         'version' => '1.0'
     ],
     'restriction_against_iframe' => [
-        'path' => '/../private/source/pages/js/restriction_against_iframe.js',
+        'path' => 'restriction_against_iframe.js',
         'version' => '1.0'
     ],
     'search_engine' => [
-        'path' => '/../private/source/pages/js/search_engine.js',
+        'path' => 'search_engine.js',
         'version' => '1.0'
     ],
     'tooltip_popover' => [
-        'path' => '/../private/source/pages/js/tooltip_popover.js',
+        'path' => 'tooltip_popover.js',
         'version' => '1.0'
     ]
 ]; 
