ad6f3f5aa5ac587beb4d397e0b38599b9301316e
MRPSouza <contato@matheusrpsouza.com>
2024-11-26 18:21:48 -0300

    Update security_headers.php

--- private/source/pages/layouts/components/html.head&startBody/php/security_headers.php
+++ private/source/pages/layouts/components/html.head&startBody/php/security_headers.php
@@ -6,15 +6,27 @@ header_remove("Content-Security-Policy-Report-Only");
 // Todas as hashes para scripts inline
 $script_hashes = [
     'sha256-jNUaLyKtUOtTVaAUziqIV9DJCNPr3ty5ZK4o7WIe1TU=',
-    'xxxx',
-    'zzzz'
+    'sha256-wShYhjL/WEbqxgBgWQPvW6VSsr1gGYvGj9ZPDNsVK3w=',
+    'sha256-k9ISLWHcPDNxxmLk/z1Kmn5JcTnyxlLgbQgQkI+zmjg=',
+    'sha256-OPlvV0PukpmbD8NgO7FYL0fXmC6HWYqI5Z4/GoE+D4w=',
+    'sha256-k2UHtayxw6rd21AKKJSQ2u7g+C9wCNMJIaWnfSFZ5Jk=',
+    'sha256-d6MhXPGKHgj5QgE+0nM7Ra+5XEQN570BMDXwhH627WM=',
+    'sha256-5G9EkZVw7e4y1kGjf2UGMPpBSj6zhFYn8xY127Ik0ZY=',
+    'sha256-ktsoVgZzBrCc7uWJHbouqdvgwtNoNKxhKFR9j81Ni0E=',
+    'sha256-gUt3x4oAYKg4ROSECT8OWrXYpMRZsKZAg6rtLgTM4wE=',
+    'sha256-nHvLboLpS88KYpzarZIWLE1txq204V4IRcpXTDbCrnA=',
+    'sha256-o+ChJmE+JVyXAsOt12Q7OHrs5Kc8TG/xTH/iNfirUlI='
 ];
 
 // Todas as hashes para estilos inline
 $style_hashes = [
     'sha256-lSQTU/F1/ZmmX3RPh56utZLWWyMEu9Uch9bC475QvPA=',
-    'yyyy',
-    'zzzz'
+    'sha256-muFRSnplr5N3iEjTOjXk+DFAY53hts6pBpEoEDnY4W0=',
+    'sha256-0qxSfKRkLT0a0s7bdPKt0OzTgaGpWK4YjJngQBs766A=',
+    'sha256-SYJUZ32nvWNLb7A/RnSwtdSuCB+zBLAtWdp1JBTfO9w=',
+    'sha256-rlx+YSsXcrPIXjRJ3Khg65SeaMFQI/6MyJvLVGaWSaA=',
+    'sha256-eoYVeC34m1+4tLjM76MjpaPlMCFnZl6oAJ+wf8124Tk=',
+    'sha256-iZkb53UPZKGjsK/QWVA4U2P7yf+8joKG6vfOAdY8pFk='
 ];
 
 // Converter arrays de hashes para string
