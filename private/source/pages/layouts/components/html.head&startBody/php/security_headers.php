2e5b35115859e025e1178751782b006ca4153e48
MRPSouza <contato@matheusrpsouza.com>
2024-11-26 18:24:43 -0300

    Update security_headers.php

--- private/source/pages/layouts/components/html.head&startBody/php/security_headers.php
+++ private/source/pages/layouts/components/html.head&startBody/php/security_headers.php
@@ -15,7 +15,8 @@ $script_hashes = [
     'sha256-ktsoVgZzBrCc7uWJHbouqdvgwtNoNKxhKFR9j81Ni0E=',
     'sha256-gUt3x4oAYKg4ROSECT8OWrXYpMRZsKZAg6rtLgTM4wE=',
     'sha256-nHvLboLpS88KYpzarZIWLE1txq204V4IRcpXTDbCrnA=',
-    'sha256-o+ChJmE+JVyXAsOt12Q7OHrs5Kc8TG/xTH/iNfirUlI='
+    'sha256-o+ChJmE+JVyXAsOt12Q7OHrs5Kc8TG/xTH/iNfirUlI=',
+    'sha256-eWZY4w0zMqvesfFrRCYEU3iTW4frlb2ZI60AeOOENUM='
 ];
 
 // Todas as hashes para estilos inline
