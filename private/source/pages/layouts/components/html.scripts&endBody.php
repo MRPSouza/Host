865f2d132e711b03e1882de9ee3f2b5d159a5be5
MRPSouza <contato@matheusrpsouza.com>
2024-11-25 22:09:33 -0300

    2024/11/25 22:08 0.1.3 scripts
    Corrigido a forma que os scripts são injetados

--- private/source/pages/layouts/components/html.scripts&endBody.php
+++ private/source/pages/layouts/components/html.scripts&endBody.php
@@ -1,10 +1,11 @@
 <?php
 // Scripts locais
+echo '<script>';
 require_once('html.scripts&endBody/js/content_dynamic.js');
 require_once('html.scripts&endBody/js/resize_body_bootstrap.js');
 require_once('html.scripts&endBody/js/restriction_against_iframe.js');
 require_once('html.scripts&endBody/js/tooltip_popover.js');
-
+echo '</script>';
 // Scripts externos com integrity
 foreach ($external_scripts as $key => $script) {
     echo "<script src='{$script['src']}' 
