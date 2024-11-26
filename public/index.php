cb5f3179a92303478e98afcd7837b87f77d94d28
MRPSouza <contato@matheusrpsouza.com>
2024-11-26 12:25:02 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,25 +4,25 @@
     $current_page = ucfirst(str_replace('-', ' ', $current_page));
     
     # Definir a visibilidade da página
-    require_once('../private/source/pages/config/page_visibility/page_public.php');
+    include_once('../private/source/pages/config/page_visibility/page_public.php');
 
     # Depois carregar o security headers que usa os nonces
-    require_once '../private/source/pages/layouts/components/html.head&startBody/php/security_headers.php';
+    include_once '../private/source/pages/layouts/components/html.head&startBody/php/security_headers.php';
 
     # Por fim carregar o HTML head que também usa os nonces
-    require_once '../private/source/pages/layouts/components/html.head&startBody.php';
+    include_once '../private/source/pages/layouts/components/html.head&startBody.php';
 
     # Incluir o cabeçalho
-    require_once '../private/source/pages/layouts/header.php';
+    include_once '../private/source/pages/layouts/header.php';
     
     # Incluir o conteúdo dinâmico
     echo '<main id="content-dynamic">' . "\n".'<!-- Início do conteúdo dinâmico -->'."\n";
-    require_once '../private/source/pages/'.$current_page.'.php';
+    include_once '../private/source/pages/'.$current_page.'.php';
     echo "\n".'<!-- Fim do conteúdo dinâmico -->'."\n".'</main>';
 
     # Incluir o rodapé
-    require_once '../private/source/pages/layouts/footer.php';
+    include_once '../private/source/pages/layouts/footer.php';
 
     # Incluir o rodapé HTML
-    require_once '../private/source/pages/layouts/components/html.scripts&endBody.php';
+    include_once '../private/source/pages/layouts/components/html.scripts&endBody.php';
 ?>
