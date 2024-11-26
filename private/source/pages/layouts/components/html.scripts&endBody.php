c6cfccd77c7f27c244b9c235ad1dd29863771443
MRPSouza <contato@matheusrpsouza.com>
2024-11-25 22:07:09 -0300

    2024/11/25 22:06 0.1.3 - scripts
    Scripts injetados na página.

--- private/source/pages/layouts/components/html.scripts&endBody.php
+++ private/source/pages/layouts/components/html.scripts&endBody.php
@@ -1,7 +1,15 @@
 <?php
 // Scripts locais
-foreach ($local_scripts as $key => $script) {
-    echo "<script src='{$script['path']}'></script>";
+require_once('html.scripts&endBody/js/content_dynamic.js');
+require_once('html.scripts&endBody/js/resize_body_bootstrap.js');
+require_once('html.scripts&endBody/js/restriction_against_iframe.js');
+require_once('html.scripts&endBody/js/tooltip_popover.js');
+
+// Scripts externos com integrity
+foreach ($external_scripts as $key => $script) {
+    echo "<script src='{$script['src']}' 
+                 integrity='{$script['integrity']}' 
+                 crossorigin='{$script['crossorigin']}'></script>";
 }
 ?>
 
