99dc154603cd6e0792a4363e7df8fe897b8f882e
MRPSouza <contato@matheusrpsouza.com>
2024-11-29 19:44:43 -0300

    2024/11/29 19:44 favicon

--- backstage/pages/layout/base_html/html.head.body.php
+++ backstage/pages/layout/base_html/html.head.body.php
@@ -16,7 +16,7 @@
     }
     ?>
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL; ?>/assets/img/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
