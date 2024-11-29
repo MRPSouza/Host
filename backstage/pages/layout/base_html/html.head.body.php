5f8bb8f130587dae18c11fec223f2dbf11e3fd4e
MRPSouza <contato@matheusrpsouza.com>
2024-11-29 19:43:15 -0300

    2024/11/29 19:43 favicon

--- backstage/pages/layout/base_html/html.head.body.php
+++ backstage/pages/layout/base_html/html.head.body.php
@@ -17,9 +17,9 @@
     ?>
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL; ?>/assets/img/favicon.ico">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo BASE_URL; ?>/assets/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo BASE_URL; ?>/assets/img/favicon-16x16.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
     <link rel="manifest" href="<?php echo BASE_URL; ?>/site.webmanifest">
     
     <!-- Previne requisições automáticas de favicon -->
