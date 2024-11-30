8dc051f7a49ba635c22413dfcaae9b945b7d8870
MRPSouza <contato@matheusrpsouza.com>
2024-11-30 01:11:02 -0300

    Update index.php

--- backstage/pages/index.php
+++ backstage/pages/index.php
@@ -32,18 +32,3 @@
 
 Início da página
 
-<?php
-// Informações do servidor
-echo "Módulos Apache carregados: <br>";
-if (function_exists('apache_get_modules')) {
-    print_r(apache_get_modules());
-}
-
-echo "<br><br>Todas as variáveis do servidor: <br>";
-foreach($_SERVER as $key => $value) {
-    echo "$key => $value <br>";
-}
-
-// Versão do PHP também pode ajudar
-echo "<br>Versão do PHP: " . PHP_VERSION;
-?>
