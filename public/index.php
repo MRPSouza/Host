cb5d5f71ee70c3cee2436a8f917223076cfb6684
MRPSouza <contato@matheusrpsouza.com>
2024-11-30 01:46:17 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
 <?php
-// Verifica www e HTTPS (versão com proxy)
-if (
-    strpos($_SERVER['HTTP_HOST'], 'www.') === 0 || 
-    (
-        (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') &&
-        (!isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || $_SERVER['HTTP_X_FORWARDED_PROTO'] !== 'https')
-    )
-) {
-    $newUrl = 'https://matheusrpsouza.com' . $_SERVER['REQUEST_URI'];
+// Força HTTPS e remove www (antes de qualquer output)
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+$isProxy = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https';
+
+if ($protocol !== 'https' && !$isProxy) {
+    $newUrl = 'https://' . str_replace('www.', '', $_SERVER['HTTP_HOST']) . $_SERVER['REQUEST_URI'];
     header("HTTP/1.1 301 Moved Permanently");
     header("Location: " . $newUrl);
     exit();
