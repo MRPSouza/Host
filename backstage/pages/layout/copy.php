39eb127eca9c5d6b92d89fcedacfff32a9ee69a3
MRPSouza <contato@matheusrpsouza.com>
2024-12-01 00:54:28 -0300

    Update copy.php

--- backstage/pages/layout/copy.php
+++ backstage/pages/layout/copy.php
@@ -1,30 +1,3 @@
-<style>
-    body {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        margin: 0;
-        justify-content: start;
-        align-items: left;
-    }
-
-    main {
-        flex: 1 0 auto;
-        display: flex;
-        justify-content: start;
-        align-items: left;
-        width: 100%;
-    }
-
-    footer {
-        margin-top: auto;
-        padding: 1rem;
-        background-color: #f8f9fa;
-        text-align: center;
-        width: 100%;
-    }
-</style>
-
 <?php
 $companyName = "Mister Cel";
 $since = "2008";
