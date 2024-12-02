100f1d8484d65e079cb617ece8b8acd055783d34
MRPSouza <contato@matheusrpsouza.com>
2024-12-02 16:19:29 -0300

    Update index.php

--- backstage/pages/index.php
+++ backstage/pages/index.php
@@ -61,16 +61,16 @@
             </div>      
         </div>
     </div>
-</section>
-
-
-<!-- Hero Section -->
-<section class="hero-section bg-gradient-success-dark py-5">
     <div class="custom-shape-divider-top-1733163571">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" class="shape-fill"></path>
         </svg>
     </div>
+</section>
+
+
+<!-- Hero Section -->
+<section class="hero-section bg-gradient-success-dark py-5">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-6">
