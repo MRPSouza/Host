bc859fb7ce05646f50f6ccfa0a451cca0e046f15
Matheus Souza <contato@matheusrpsouza.com>
2024-12-18 19:16:44 -0300

    Delete SecretPath directory

--- SecretPath/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-# Domínio confiável
-$trustedDomain = 'matheusrpsouza.com'; 
-
-# Diretório raiz do projeto
-define('ROOT_DIR', dirname(__DIR__));
-define('BASE_URL', 'https://'.$trustedDomain);
--- SecretPath/templates/loader/loader.php
+++
@@ -1,6 +0,0 @@
-<div id="page-loader" class="loader-wrapper">
-    <div class="loader">
-        <div class="spinner"></div>
-        <div class="loading-text">Carregando...</div>
-    </div>
-</div> 
