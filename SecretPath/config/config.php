ab38a047a3761ab04647f5e10f807005170f968d
MRPSouza <contato@matheusrpsouza.com>
2024-12-18 17:53:50 -0300

    Update config.php

--- SecretPath/config/config.php
+++ SecretPath/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 # Domínio confiável
-$trustedDomain = '127.0.0.1/Dashboard/Public/'; 
+$trustedDomain = 'matheusrpsouza.com'; 
 
 # Diretório raiz do projeto
 define('ROOT_DIR', dirname(__DIR__));
