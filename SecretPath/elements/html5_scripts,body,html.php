79ae406884ea0ffa2e4100a377e66cce12e7dd46
MRPSouza <contato@matheusrpsouza.com>
2024-12-19 12:12:40 -0300

    Otimização JS (defer/async) e CSP

--- SecretPath/elements/html5_scripts,body,html.php
+++ SecretPath/elements/html5_scripts,body,html.php
@@ -3,15 +3,14 @@
 include_once '../'.$rootPrivate.'/config/config.php';
 
 echo
-'<script src="js/loader.js" type="application/javascript"></script>'.
-'<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>'.
-'<script src="https://code.jquery.com/jquery-3.7.1.min.js" 
+'<script async src="js/loader.js" type="application/javascript"></script>'.
+'<script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>'.
+'<script defer src="https://code.jquery.com/jquery-3.7.1.min.js" 
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" 
         crossorigin="anonymous"></script>'.
 '<script src="js/script_main_header.js" type="application/javascript"></script>'.
-'<script src="js/ajax-navigation.js" type="application/javascript"></script>'.
-'<script src="js/phone-links.js" type="application/javascript"></script>'.
-'<script src="js/home.js" type="application/javascript"></script>'.
+'<script src="js/script_main_footer.js" type="application/javascript"></script>'.
+'<script defer src="js/ajax-navigation.js" type="application/javascript"></script>'.
 '<script>'.
 '';
 
